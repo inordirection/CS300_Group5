6ab975f75349ae337910269c617691c3a1469554
inordirection <crobetorye@gmail.com>
2019-03-10 20:45:02 -0700

    supplies percent change

--- script/ship.js
+++ script/ship.js
@@ -70,9 +70,9 @@ function Ship(json) {
 	// use the standard 2% of supplies
 	this.useSupplies = function() {
 		// decrease by 2% (to 2 decimal precision)
-		/*this.supplies = Math.round(this.supplies * 98);
-		this.supplies /= 100;*/
-		this.supplies -= 2;
+		this.supplies = Math.round(this.supplies * 98);
+		this.supplies /= 100;
+		//this.supplies -= 2;
 	}
 
 	this.useEnergy = function(used) {
